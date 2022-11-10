6cc3a9e1ebaa47560a5d64f1eabdda64b3b7de0b
stephano montes <112264544+stephanomontes@users.noreply.github.com>
2022-11-09 21:00:18 -0500

    Add files via upload

--- mysite/myapi/urls.py
+++ mysite/myapi/urls.py
@@ -1,9 +1,13 @@
+
 # myapi/urls.py
 from django.urls import include, path
 from rest_framework import routers
 from . import views
 from myapi.views import TopSellingBooksViewSet
 from myapi.views import GenreListsViewSet
+from myapi.views import ISBNListsViewSet
+from myapi.views import RatingListsViewSet
+from myapi.views import BookByAuthorListsViewSet
 from myapi.views import TopRatedBooksViewSet
 from myapi.views import AverageRatingViewSet
 
@@ -17,6 +21,8 @@ router.register(r'Bookratings', views.BookratingsViewSet, 'Bookratings')
 router.register(r'Purchasedbooks', views.PurchasedbooksViewSet, 'Purchasedbooks')
 router.register(r'Users', views.UsersViewSet, 'Users')
 router.register(r'Creditcards', views.CreditcardsViewSet, 'Creditcards')
+router.register(r'Shoppingcarts', views.ShoppingcartsViewSet, 'Shoppingcarts')
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
@@ -24,8 +30,10 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('Books/TopSellers/', TopSellingBooksViewSet.as_view()),
     path('Books/Genre/', GenreListsViewSet.as_view()),
+    path('Books/ISBN/', ISBNListsViewSet.as_view()),
+    path('Books/Author/', BookByAuthorListsViewSet.as_view()),
+    path('Books/Rating/', RatingListsViewSet.as_view()),
     path('Bookratings/TopRated/', TopRatedBooksViewSet.as_view()),
     path('Bookratings/AverageRating/', AverageRatingViewSet.as_view()),
     path('', include(router.urls))
-
 ]
